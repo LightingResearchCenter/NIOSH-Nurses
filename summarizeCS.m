78dee4a02efec4f40bc7d729e4ff476012ae669c
Charlie Roohan <roohac2@rpi.edu>
2018-01-23 16:54:23 -0500

    Add condition cross reference

--- summarizeCS.m
+++ summarizeCS.m
@@ -6,15 +6,11 @@ function summarizeCS
 timestamp = datestr(now,'yyyy-mm-dd_HHMM');
 % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-
-
 % Enable dependencies ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 [githubDir,~,~] = fileparts(pwd);
 d12packDir = fullfile(githubDir,'d12pack');
 addpath(d12packDir);
 % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-
 
 % Map project folder paths ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 projectDir = '\\root\projects\NIOSH_RedLightForShiftWorkers\daysimeter_data';
@@ -42,13 +38,45 @@ nObj = numel(objArray);
 h = waitbar(0,'Please wait. Analyzing data...');
 
 tb = table;
+tb.id = regexprep({objArray.ID}','(\d\d\d).*','$1');
+tb.color = cell(nObj,1);
+tb.protocol = cell(nObj,1);
+tb.shift = cell(nObj,1);
 tb.mean_valid_CS = nan(nObj,1);
 tb.category = cell(nObj,1);
-tb.Properties.RowNames = regexprep({objArray.ID}','(\d\d\d).*','$1');
 tb.Properties.DimensionNames{1} = ['file_',sheet];
+
+subjectPath = '\\root\projects\NIOSH_RedLightForShiftWorkers\daysimeter_data\SubjectConditions.xlsx';
+%% Load data
+S = readtable(subjectPath);
+S(isnan(S.ids),:) = [];
+S.startTime.TimeZone = 'America/New_York';
+S.stopTime.TimeZone = 'America/New_York';
+%%
 
 for iObj = 1:nObj
     obj = objArray(iObj);
+    
+    thisS = S(S.ids == str2double(objArray(iObj).ID) & strcmp(S.sn,num2str(objArray(iObj).SerialNumber)),:);
+    TF = false(size(thisS.ids));
+    for iS = 1:height(thisS)
+        TF(iS) = timeBetween(objArray(iObj).Time, thisS.startTime(iS), thisS.stopTime(iS));
+    end
+    thisS = thisS(TF, :);
+    
+    if ~isempty(thisS)
+        objArray(iObj).Session(1).Color    = thisS.color_period(1);
+        objArray(iObj).Session(1).Protocol = thisS.protocol(1);
+        objArray(iObj).Session(1).Shift = thisS.shift(1);
+    else
+        objArray(iObj).Session(1).Color    = 'unknown';
+        objArray(iObj).Session(1).Protocol = 'unknown';
+        objArray(iObj).Session(1).Shift = 'unknown';
+    end
+    
+    tb.color{iObj} = objArray(iObj).Session(1).Color;
+    tb.protocol{iObj} = objArray(iObj).Session(1).Protocol;
+    tb.shift{iObj} = objArray(iObj).Session(1).Shift;
     
     idxValid = obj.Observation & ~obj.InBed & obj.Compliance & ~obj.Error;
     
@@ -66,8 +94,14 @@ for iObj = 1:nObj
     
     waitbar(iObj/nObj);
 end
-writetable(tb,savePath,'Sheet',sheet,'WriteVariableNames',true,'WriteRowNames',true);
+writetable(tb,savePath,'Sheet',sheet,'WriteVariableNames',true,'WriteRowNames',false);
 close(h);
 
+
+winopen(savePath)
 end
 
+
+function TF = timeBetween(Time, startTime, stopTime)
+TF = any(Time >= startTime & Time <= stopTime);
+end
